ca3f2837b6553eb40f20a62e1fbbbb2d3de45024
Sandoval <ismael.sandoval.aguilar@gmail.com>
2022-08-15 20:40:38 +0000

    Cambios actividad

--- estructura_de_datos/actividad_aprendizaje_1.cpp
+++ estructura_de_datos/actividad_aprendizaje_1.cpp
@@ -9,28 +9,33 @@ en memoria.
 
 */ 
 
+// Librerias necesarias 
 #include <iostream>
 #include <new>
-
 using namespace std;
 
+// Declaración de la función principal
 int main()
 {
+    // Asignación de variables
     int n = 0;
     int i = 0;
 
-    cout << "Introducir el tamaño del arreglo\n:";
+    // El usuario introduce el valor de "n"
+    cout << "Introducir el tamaño del arreglo:\n";
     cin >> n; 
 
+    // Declaración del puntero "arreglo" en memoria dinámica
     int *arreglo = new int[n];
 
+    // Itera "n" veces e incrementa de 10 en 10
     for (i = 0; i < n; i++) 
     {
         arreglo[i] = (i + 1) * 10;
     }
 
+    // Itera "n" veces e imprime el contenido del arreglo
     cout << "Elementos del arreglo:\n";
-
     for (i = 0; i < n; i++) 
     {
         cout << arreglo[i] << " ";
@@ -38,6 +43,7 @@ int main()
 
     cout << endl;
 
+    // Libera el espacio en memoria dinámica
     delete[] arreglo;
 
     return 0;
