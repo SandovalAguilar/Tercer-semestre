bc1a15a37a39df0deecab510a970362ffe2876e0
SandovalAguilar <ismael.sandoval.aguilar@gmail.com>
2022-08-15 19:09:03 -0500

    edicion

--- estructura_de_datos/actividad_1.c
+++ estructura_de_datos/actividad_1.c
@@ -9,7 +9,29 @@ void sumatoria_funciones();
 
 int main()
 {
-    suma_funciones();
+    int n;
+    int opcion;
+
+    do
+    {
+        printf("\n   1. Sumar dos arreglos.\n");
+        printf("\n   2. Obtener la sumatoria de los elementos de un arreglo.\n");
+        printf("\n   3. Salir.\n" );
+        printf("\n\n   Introduzca opcion (1-3): ");
+
+        scanf( "%d", &opcion );
+
+        switch (opcion)
+        {
+            case 1:
+                suma_funciones();
+                break;
+            case 2:
+                sumatoria_funciones();
+                break;
+        }
+
+    } while (opcion != 3);
     
     return 0;
 }
@@ -50,4 +72,24 @@ void suma_funciones()
     {
         printf("%d ", C[i]);
     }   
+}
+
+void sumatoria_funciones()
+{
+    int D[20] = {};
+    int i = 0;
+    int total = 10;
+
+    for (i = 0; i < 20; i++)
+    {
+        printf("Ingresar el elemento #%d del arreglo:\n", i + 1);
+        scanf("%d", &D[i]);
+    }
+
+    for (i = 0; i < 20; i++)
+    {
+        total = total + D[i];
+    }
+
+    printf("La sumatoria de todos los elementos del arreglo es: %d\n", total);
 }
